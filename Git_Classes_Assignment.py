b24942866a0ee28fb3a6bfbedf34aa5cf56fd254
LORD-RAID3N <jonahpeter96@gmail.com>
2022-08-23 21:42:33 +0100

    Nasooo

---
+++ Git_Classes_Assignment.py
@@ -0,0 +1,25 @@
+class Student:
+
+    def __init__(self, f_name, l_name, level):
+        self.first_name = f_name
+        self.last_name = l_name
+        self.email = f_name + "." + l_name + "@codeplateau.com"
+        self.level = level
+
+
+class teachers(Student):
+    def __init__(self, f_name, l_name, level, prog):
+        super().__init__(f_name, l_name, level)
+        self.prog = prog
+
+
+
+
+student1 = Student("Jonah", "Peter", 300)
+student2 = Student("Victor", "Pictda", 200)
+
+Teacher1 = teachers("Fifth", "Son", "Code Plateau_3.0", "Python")
+Teacher2 = teachers("Lucky", "Yoila", "Code Plateau_2.0", "Ubuntu")
+
+
+print(Teacher2.level)
